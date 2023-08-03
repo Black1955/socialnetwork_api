3378dd895b0fbc59abe7632dce0115504d471254
Illia <ichorny.ilya25@gmail.com>
2023-08-03 22:03:33 +0200

    awdwad

--- routers/user.router.js
+++ routers/user.router.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import userController from "../controllers/user.controller.js";
 import tokenMiddleware from "../middlewares/token.middleware.js";
 import { upload } from "../middlewares/file.middleware.js";
+import pool from "../db.js";
 export const userRouter = Router();
 
 userRouter.post("/signin", userController.signin);
@@ -9,6 +10,14 @@ userRouter.post("/signup", userController.signup);
 userRouter.get("/refresh", tokenMiddleware, userController.refresch);
 userRouter.get("/getuser/:id", tokenMiddleware, userController.getUser);
 userRouter.post("/subscribe", tokenMiddleware, userController.SubscribeUser);
+userRouter.get("/oleg", async (req, res) => {
+  try {
+    const oleg = await pool.query("select * from users");
+    return res.json(oleg.rows);
+  } catch (error) {
+    return res.json(error);
+  }
+});
 userRouter.post(
   "/unsubscribe",
   tokenMiddleware,
